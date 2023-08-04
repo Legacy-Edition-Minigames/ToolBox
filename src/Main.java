2a94a0dc92ac7986b47672a2dcd433f53a7251f5
niceEli <EliCardoso2009@gmail.com>
2023-08-04 18:55:48 -0400

    add starter stuff ig

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        for (String arg : args) {
-            OUTKAT.executeScript(arg);
+        if(args.length == 0){
+            System.out.println("dont worry its not broken your just accessing something that isn't done yet, you can use args such as --inject to run an OUTKAT file or gui to try and run the gui");
+        } else if (args[0].equals("--inject")) {
+            OUTKAT.executeScript(args[1]);
+        } else if (args[0].equals("gui")){
+            mainMenu.GUI();
         }
     }
 }
---
+++ src/mainMenu.java
@@ -0,0 +1,6 @@
+public class mainMenu {
+    public static void GUI(){
+        System.out.println("Work In Progress");
+        System.out.println("Enjoy Having This Beta Build");
+    }
+}
