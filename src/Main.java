432afe81b82077ffa8ca83641faee26583b90fa7
niceEli <EliCardoso2009@gmail.com>
2023-08-05 11:26:34 -0400

    FLOPPA

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import GUIIcons.Floppa;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -8,8 +10,11 @@ public class Main {
             System.out.println("dont worry its not broken your just accessing something that isn't done yet, you can use args such as --inject to run an OUTKAT file or gui to try and run the gui");
         } else if (args[0].equals("--inject")) {
             OUTKAT.executeScript(args[1]);
-        } else if (args[0].equals("gui")){
+        } else if (args[0].equals("gui")) {
             mainMenu.GUI();
+
+        } else if (args[0].equals("floppa")) {
+            Floppa.GUI();
         } else {
             System.out.println("Available Args:");
             System.out.println("empty");
