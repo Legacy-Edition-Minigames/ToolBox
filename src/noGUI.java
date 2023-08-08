a24efd9c8451cffb175647791800d4795faeccc4
niceEli <EliCardoso2009@gmail.com>
2023-08-08 08:56:41 -0400

    goofy bug whre reinstall every open
    fixed

--- src/noGUI.java
+++ src/noGUI.java
@@ -1,11 +1,13 @@
 import java.io.IOException;
+import java.util.Objects;
 
 public class noGUI {
 
     public static void GUI() throws IOException {
         Conf.createIfNotExists();
         FileDownloader.main(new String[]{Conf.get(2) + "/" + Conf.get(0) + "/main.ok", "./main.ok"});
-        if (Conf.get(1) == ReadSpecificLine.main(new String[]{"./main.ok", "1"})){
+        if (Objects.equals(Conf.get(1), ReadSpecificLine.main(new String[]{"./main.ok", "1"}))){
+            Conf.set("1", ReadSpecificLine.main(new String[]{"./main.ok", "1"}));
             OUTKAT.executeScript("./main.ok");
         }
         FileDownloader.main(new String[]{Conf.get(2) + "/" + Conf.get(0) + "/CRC", "./CRC"});
