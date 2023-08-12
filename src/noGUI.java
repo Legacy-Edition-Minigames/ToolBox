d1b11eeb76f3209959618dde7f77e206b33a213d
niceEli <EliCardoso2009@gmail.com>
2023-08-11 21:10:03 -0400

    OUTKAT FIX 2

--- src/noGUI.java
+++ src/noGUI.java
@@ -3,15 +3,22 @@ import java.util.Objects;
 
 public class noGUI {
 
-    public static void GUI() throws IOException {
-        Conf.createIfNotExists();
-        FileDownloader.main(new String[]{Conf.get(2) + "/" + Conf.get(0) + "/main.ok", "./main.ok"});
-        if (Objects.equals(Conf.get(1), ReadSpecificLine.main(new String[]{"./main.ok", "1"}))){
-            Conf.set("1", ReadSpecificLine.main(new String[]{"./main.ok", "1"}));
-            OUTKAT.executeScript("./main.ok");
-        }
-        FileDownloader.main(new String[]{Conf.get(2) + "/" + Conf.get(0) + "/CRC", "./CRC"});
-        FileWorkerThreads.RUNCRC("./CRC");
-    }
+    public static void GUI() {
+        try {
+            Conf.createIfNotExists();
+            FileDownloader.main(new String[]{Conf.get(2) + "/" + Conf.get(0) + "/main.ok", "./main.ok"});
 
+            String confValue = ReadSpecificLine.main(new String[]{"./main.ok", "1"});
+            if (Objects.equals(Conf.get(1), confValue)) {
+                Conf.set("1", confValue);
+                OUTKAT.executeScript("./main.ok");
+            }
+
+            FileDownloader.main(new String[]{Conf.get(2) + "/" + Conf.get(0) + "/CRC", "./CRC"});
+            FileWorkerThreads.RUNCRC("./CRC");
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception here, if needed
+        }
+    }
 }
