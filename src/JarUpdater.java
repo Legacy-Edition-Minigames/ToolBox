08254cbcb87ddcb4b770d65fced966c6d380100a
niceEli <EliCardoso2009@gmail.com>
2023-08-11 20:45:54 -0400

    S*** I forgot to change the metadata

--- src/JarUpdater.java
+++ src/JarUpdater.java
@@ -4,7 +4,7 @@ import java.net.URL;
 
 public class JarUpdater {
 
-    private static final String CURRENT_VERSION = "1.0.0";
+    private static final String CURRENT_VERSION = "1.1.0";
 
     public static void update() {
         String jarFilePath = getJarFilePath();
